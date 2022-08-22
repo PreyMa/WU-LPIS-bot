15ac291d0a71b753fb687d2fb7ed4802714141fb
PreyMa <mlpreymann@gmail.com>
2022-08-22 23:55:40 +0200

    Fix page matching for http pages
    This is temporary anyway

--- wubot.js
+++ wubot.js
@@ -4,8 +4,7 @@
 // @version      0.1
 // @description  Register with ease
 // @author       PreyMa
-// @match        http://*/*
-// @match        https://*/*
+// @match        *://*/*
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgBAMAAACBVGfHAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAASUExURQAAAH9/f9PT0+0cJO/v7////4KSIg8AAACKSURBVCjPhdFRDoAgCABQr4DhAewGjXUANi/QR/e/SoqkVLr40HyDZOhSDSL9cLrvb6BfYDQAAMjIeVNYiAoQbRMAEwJ+bREVlGKDnJuPeYmzEsmwEM7jWRKcBdYMKcEK/R8FQG6JdYURsO0DR9A7Bf9qPXjTeokOQWMO9wD9ZB6fIcvD1VdKA7gAUO5YI8LDmx0AAAAASUVORK5CYII=
 // @grant        GM_setValue
 // @grant        GM_getValue
