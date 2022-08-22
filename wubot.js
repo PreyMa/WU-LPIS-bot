0d6902134448481739aac5ea46ba7a90c4fd7ee3
PreyMa <mlpreymann@gmail.com>
2022-08-22 21:50:26 +0200

    Add metadata block to example code

---
+++ wubot.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         WU LPIS Registration Bot
+// @namespace    https://www.egimoto.com
+// @version      0.1
+// @description  Register with ease
+// @author       PreyMa
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
