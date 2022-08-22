b0bde7c03fd35d948c452512ffc531df49c0a601
PreyMa <mlpreymann@gmail.com>
2022-08-22 22:11:24 +0200

    Grant storage access and disallow injection into frames

--- wubot.js
+++ wubot.js
@@ -7,7 +7,9 @@
 // @match        http://*/*
 // @match        https://*/*
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgBAMAAACBVGfHAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAASUExURQAAAH9/f9PT0+0cJO/v7////4KSIg8AAACKSURBVCjPhdFRDoAgCABQr4DhAewGjXUANi/QR/e/SoqkVLr40HyDZOhSDSL9cLrvb6BfYDQAAMjIeVNYiAoQbRMAEwJ+bREVlGKDnJuPeYmzEsmwEM7jWRKcBdYMKcEK/R8FQG6JdYURsO0DR9A7Bf9qPXjTeokOQWMO9wD9ZB6fIcvD1VdKA7gAUO5YI8LDmx0AAAAASUVORK5CYII=
-// @grant        none
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @noframes
 // ==/UserScript==
 
 (function() {
